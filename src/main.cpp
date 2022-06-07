8c8c377b694bc22f03009c77ba35777d7ee2aee8
Andrew McWatters <mcwattersandrew@gmail.com>
2022-06-07 10:35:47 -0700

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+int main() { }     // the minimal C++ program
+
+// Excerpt From
+// The C++ Programming Language, Fourth Edition
+// Bjarne Stroustrup
+// This material may be protected by copyright.
