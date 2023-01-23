bbb21860f3ed4c9ab0c9599995dff7f9b0a79542
Andrew McWatters <mcwattersandrew@gmail.com>
2023-01-23 08:50:41 -0700

    Add `atexit()` call to `filesystem_shutdown()`

--- src/filesystem.c
+++ src/filesystem.c
@@ -1,10 +1,13 @@
 /* Copyright Planimeter. All Rights Reserved. */
 
 #include "physfs.h"
+#include "filesystem.h"
 
 void filesystem_init(const char *argv0)
 {
     PHYSFS_init(argv0);
+
+    atexit(filesystem_shutdown);
 }
 
 void filesystem_shutdown()
