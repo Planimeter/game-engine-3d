9423997a569dc739259336113fb1ae94c1c3c46a
Andrew McWatters <mcwattersandrew@gmail.com>
2023-01-23 21:56:38 -0700

    Update `filesystem_init()`

--- src/filesystem_null.c
+++ src/filesystem_null.c
@@ -1,7 +1,12 @@
 /* Copyright Planimeter. All Rights Reserved. */
 
+#include <stdlib.h>
+
 void filesystem_init(const char *argv0)
 {
+    void filesystem_shutdown();
+
+    atexit(filesystem_shutdown);
 }
 
 void filesystem_shutdown()
