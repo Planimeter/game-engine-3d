ab08dc5acc8ad7dc557a803800d85bd29bea2892
Andrew McWatters <mcwattersandrew@gmail.com>
2023-01-24 13:13:11 -0700

    Port `filesystem` interface to POSIX

---
+++ src/filesystem_posix.c
@@ -0,0 +1,53 @@
+/* Copyright Planimeter. All Rights Reserved. */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+/* fsize:  return size of file "name" */
+static off_t fsize(char *name, FILE *stream)
+{
+    struct stat stbuf;
+
+    if (fstat(stream, &stbuf) == -1) {
+        fprintf(stderr, "fsize: can't access %s\n", name);
+        return -1;
+    }
+    return stbuf.st_size;
+}
+
+void filesystem_init(const char *argv0)
+{
+    void filesystem_shutdown();
+
+    atexit(filesystem_shutdown);
+}
+
+char *filesystem_fileread(const char *pathname)
+{
+    FILE *fp;
+    off_t size;
+    char *p;
+    size_t elements_read;
+
+    if ((fp = fopen(pathname, "rb")) == NULL)
+        return NULL;
+    size = fsize(pathname, fp);
+    p = (char *) malloc(size+1);  /* +1 for ′\0′ */
+    if (p == NULL) {
+        fclose(fp);
+        return NULL;
+    }
+    elements_read = fread(p, size, 1, fp);
+    if (elements_read != 1) {
+        free(p);
+        fclose(fp);
+        return NULL;
+    }
+    fclose(fp);
+    return p;
+}
+
+void filesystem_shutdown()
+{
+}
