7533e493cd90b87bf8e786eff7928b4b3d55cc28
Andrew McWatters <mcwattersandrew@gmail.com>
2023-01-24 17:39:54 -0700

    Format Document

--- src/filesystem.h
+++ src/filesystem.h
@@ -7,9 +7,9 @@
 extern "C" {
 #endif
 
-void filesystem_init(const char *argv0);
+void  filesystem_init(const char *argv0);
 char *filesystem_fileread(const char *pathname);
-void filesystem_shutdown(void);
+void  filesystem_shutdown(void);
 
 #ifdef __cplusplus
 }
