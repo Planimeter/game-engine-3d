88653bfc5dbadc44b230e47548abf272c7d44340
Andrew McWatters <mcwattersandrew@gmail.com>
2023-02-18 16:40:37 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,18 +5,34 @@
 #include "timer.h"
 #include "graphics.h"
 
-int main(int argc, char *argv[])
+static void load(int argc, char *argv[])
 {
     framework_init(argv[0]);
     framework_load(argc, argv);
+}
+
+static void update()
+{
+    uint64_t dt = timer_step();
+    framework_update(dt);
+}
+
+static void draw()
+{
+    graphics_predraw();
+    framework_draw();
+    graphics_postdraw();
+    graphics_present();
+}
+
+int main(int argc, char *argv[])
+{
+    load(argc, argv);
 
     while (event_poll()) {
-        uint64_t dt = timer_step();
-        framework_update(dt);
-        graphics_predraw();
-        framework_draw();
-        graphics_postdraw();
-        graphics_present();
+        update();
+        draw();
+
         timer_sleep(1);
     }
 
