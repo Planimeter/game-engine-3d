7a7b035a482c5339d1d59a913a6f4f9f3ac4ff40
Andrew McWatters <mcwattersandrew@gmail.com>
2023-07-28 16:02:12 -0700

    Copy graphics_null.c to graphics_opengl.c

---
+++ src/graphics_opengl.c
@@ -0,0 +1,62 @@
+/* Copyright Planimeter. All Rights Reserved. */
+
+#include "graphics.h"
+#include <stddef.h>
+#include <stdio.h>
+
+void graphics_init()
+{
+    void graphics_shutdown(void);
+
+    atexit(graphics_shutdown);
+}
+
+Shader graphics_createshader(const char *shader, size_t size)
+{
+    return NULL;
+}
+
+void graphics_destroyshader(Shader shader)
+{
+}
+
+int graphics_isminimized()
+{
+    return 0;
+}
+
+void graphics_predraw()
+{
+    if (graphics_isminimized())
+    {
+        return;
+    }
+}
+
+void graphics_postdraw()
+{
+    if (graphics_isminimized())
+    {
+        return;
+    }
+}
+
+void graphics_present()
+{
+    if (graphics_isminimized())
+    {
+        return;
+    }
+}
+
+void graphics_resize()
+{
+}
+
+void graphics_setshader(Shader vertShader, Shader fragShader)
+{
+}
+
+void graphics_shutdown(void)
+{
+}
