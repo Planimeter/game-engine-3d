0053580c0a281744a144f60652004015f9308c92
Andrew McWatters <mcwattersandrew@gmail.com>
2023-07-28 10:45:19 -0700

    Create main_sdl.c

---
+++ src/main_sdl.c
@@ -0,0 +1,41 @@
+/* Copyright Planimeter. All Rights Reserved. */
+
+#include "SDL.h"
+#include "framework.h"
+#include "event.h"
+#include "timer.h"
+#include "graphics.h"
+
+static void load(int argc, char *argv[])
+{
+    framework_init(argv[0]);
+    framework_load(argc, argv);
+}
+
+static void update()
+{
+    uint64_t dt = timer_step();
+    framework_update(dt);
+}
+
+static void draw()
+{
+    graphics_predraw();
+    framework_draw();
+    graphics_postdraw();
+    graphics_present();
+}
+
+int main(int argc, char *argv[])
+{
+    load(argc, argv);
+
+    while (event_poll()) {
+        update();
+        draw();
+
+        timer_sleep(1);
+    }
+
+    return 0;
+}
