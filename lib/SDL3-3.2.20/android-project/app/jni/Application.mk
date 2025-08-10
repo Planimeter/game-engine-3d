2c708a8201f1b995ad51c6c511ccadd29eb38705
Andrew McWatters <andrew@planimeter.org>
2025-08-09 18:44:09 -0700

    Update SDL to 3.2.20

---
+++ lib/SDL3-3.2.20/android-project/app/jni/Application.mk
@@ -0,0 +1,13 @@
+
+# Uncomment this if you're using STL in your project
+# You can find more information here:
+# https://developer.android.com/ndk/guides/cpp-support
+# APP_STL := c++_shared
+
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+
+# Min runtime API level
+APP_PLATFORM=android-21
+
+# https://developer.android.com/guide/practices/page-sizes#update-packaging
+APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
